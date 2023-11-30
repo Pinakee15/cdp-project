e2fe1bceb4fd402bf8bea67fef325e795c10cb19
PinakeeP <pk@Pinakees-MacBook-Pro.local>
2023-12-01 02:37:27 +0530

    second commit

--- src/Components/DirectoryPicker.js
+++ src/Components/DirectoryPicker.js
@@ -4,46 +4,63 @@ import { v4 as uuidv4 } from "uuid";
 const FilePicker = () => {
   const handlePickFolder = async () => {
     const directoryHandle = await window.showDirectoryPicker();
+    const mainPicture = [];
+    const mainSound = [];
+    const mainSubtitle = [];
+    const allowedFileTypes = [
+      "video/mp4",
+      "audio/mpeg",
+      "audio/mp3",
+      "text/plain",
+    ];
     for await (const [name, entry] of directoryHandle.entries()) {
       try {
         const file = await entry.getFile();
-        const fileSize = file.size;
-        const chunkSize = 10; // Number of chunks
+        if (allowedFileTypes.includes(file.type)) {
+          const fileSize = file.size;
+          const chunkSize = 10; // Number of chunks
 
-        const chunks = [];
-        const chunkSizeInBytes = Math.ceil(fileSize / chunkSize);
-        console.log({ name });
-        for (let i = 0; i < chunkSize; i++) {
-          const offset = i * chunkSizeInBytes;
-          const end = Math.min(offset + chunkSizeInBytes, fileSize);
-          const blobChunk = file.slice(offset, end);
-          const reader = new FileReader();
+          const chunkSizeInBytes = Math.ceil(fileSize / chunkSize);
+          for (let i = 0; i < chunkSize; i++) {
+            const offset = i * chunkSizeInBytes;
+            const end = Math.min(offset + chunkSizeInBytes, fileSize);
+            const blobChunk = file.slice(offset, end);
+            const reader = new FileReader();
 
-          reader.onload = async function () {
-            const fileContent = reader.result;
-            const hash = await calculateHash(fileContent);
-            const chunkUUID = uuidv4();
-            chunks.push({
-              [`${name}_part${i + 1}`]: fileContent,
-              hash,
-              chunkUUID,
-            });
+            reader.onload = async function () {
+              const fileContent = reader.result;
+              const hash = await calculateHash(fileContent);
+              const chunkUUID = uuidv4();
+              let chunkFile = {
+                [`${name}_part${i + 1}`]: fileContent,
+                hash,
+                chunkUUID,
+              };
+              if (file.type === "video/mp4") {
+                const path = `highsmpte-reel-${i}-jp2k.mxf`;
+                mainPicture.push({ ...chunkFile, path, annotationText: path });
+              } else if (file.type === "text/plain") {
+                const path = `highsmpte-reel-${i}-pcm.mxf`;
+                mainSubtitle.push({ ...chunkFile, path, annotationText: path });
+              } else {
+                const path = `highsmpte-reel-${i}.mxf`;
+                mainSound.push({ ...chunkFile, path, annotationText: path });
+              }
+            };
 
-            if (chunks.length === chunkSize) {
-              console.log(`File ${name} divided into chunks:`, chunks);
-            }
-          };
+            reader.onerror = function (err) {
+              console.error(`Error reading ${name}:`, err);
+            };
 
-          reader.onerror = function (err) {
-            console.error(`Error reading ${name}:`, err);
-          };
-
-          reader.readAsArrayBuffer(blobChunk);
-        }
+            reader.readAsArrayBuffer(blobChunk);
+          }
+        } else continue;
       } catch (err) {
         console.error(`Error fetching ${name}:`, err);
+        throw new Error("Some error occurred");
       }
     }
+    return { mainPicture, mainSound, mainSubtitle };
   };
 
   const calculateHash = async (data) => {
@@ -54,9 +71,14 @@ const FilePicker = () => {
       .join("");
   };
 
+  const generateCDP = async () => {
+    const { mainPicture, mainSound, mainSubtitle } = await handlePickFolder();
+    console.log({ mainPicture, mainSound, mainSubtitle }, "finally");
+  };
+
   return (
     <div>
-      <button onClick={handlePickFolder}>Select Folder</button>
+      <button onClick={generateCDP}>Select Folder</button>
     </div>
   );
 };
